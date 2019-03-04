192cac88ac88419e6953a256adb46e76d8f3f5a3
Ioana Bostan <ioanabtn@yahoo.com>
2019-03-04 22:43:07 +0200

    leading and ending spaces

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function run() {
 	
-	var first_operand = document.getElementById('first').value;
-	var second_operand = document.getElementById('second').value;
-	var third_operand = document.getElementById('third').value;
-	var fourth_operand = document.getElementById('fourth').value;
+	first_operand = document.getElementById('first').value;
+	second_operand = document.getElementById('second').value;
+	third_operand = document.getElementById('third').value;
+	fourth_operand = document.getElementById('fourth').value;
 
 	if (isInteger(first_operand) && isInteger(second_operand) && isInteger(third_operand) && isInteger(fourth_operand)) {
-		var result = parseInt(first_operand) + parseInt(second_operand) + parseInt(third_operand)  * parseInt(fourth_operand) ;
+		result = parseInt(first_operand) + parseInt(second_operand) + parseInt(third_operand)  * parseInt(fourth_operand) ;
 		document.querySelectorAll('span')[1].innerHTML = parseInt(result);
 		document.querySelectorAll('span')[0].innerHTML = 'no error';
 	} else {
@@ -17,7 +17,7 @@ function run() {
 }
 
 function isInteger(number) {
-	var regex = /^([+-]?[1-9]\d*|0)$/;
+	var regex = /^(\s?[+-]?[1-9]\d*\s?|0)$/;
 	return number.match(regex);
 }
 
